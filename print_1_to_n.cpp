0e9d7b75d6811a2127ab13dba8217a1204c01be1
Vaishali_Vala <vaishalivala@gmail.com>
2025-07-15 21:49:15 +0530

    first commit

---
+++ print_1_to_n.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //  Enter  : 5
+    //  Output : 1, 2, 3, 4, 5 
+    int i = 1, n;
+
+    cout << "Enter the n Number for print 1 to n Numbers : " ;
+    cin >> n;
+
+    while(i<=n){
+        cout << i << " " ;
+        i++;
+    }
+
+    return 0;
+}
