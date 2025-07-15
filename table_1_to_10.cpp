c1c348b60b4bc6a9cd7d1d3a8ff4efb0f9353a82
Vaishali Vala <vaishali123@gmail.com>
2025-07-15 23:40:47 +0530

    Fourth File

---
+++ table_1_to_10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Enter the Number for Print the Table : " ;
+    cin >> n;
+
+    for(int i=1; i<=10; i++){
+        cout << n << " * " << i << " = " << n*i << endl;
+    }cout << endl;
+
+    return 0;
+}
