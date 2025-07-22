58e1efe9c70d0ae03060f5bb5fe5f94bb80082ee
Vaishali Vala <vaishali123@gmail.com>
2025-07-22 23:23:17 +0530

    First commit

---
+++ prime.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool isPrime = true;
+
+    cout << "Enter a number: ";
+    cin >> n;   // 2
+
+    if (n <= 1) {
+        isPrime = false;
+    } else {
+        for (int i = 2; i <= n / 2; i++) {  // 2 <= 2/2= 0 , loop  2 to 0
+            if (n % i == 0) {   // 2%2==0 
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime) {
+        cout << "The number is Prime Number." << endl;
+    } else {
+        cout << "The number is Not Prime Number." << endl;
+    }
+
+    return 0;
+}
